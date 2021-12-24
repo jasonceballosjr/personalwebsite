914c185f9012b18fd6aee6e62651aca712d69121
Jason Ceballos <jceballos@icstars.org>
2021-12-23 23:36:26 -0600

    December Commit

--- src/components/Works/index.jsx
+++ src/components/Works/index.jsx
@@ -1,12 +1,22 @@
 import "./style.css";
+import gelogo from '../../pics/gelogo.png';
+import icstars from '../../pics/ics.png';
 
 const About = () => {
     return (
         <div className="works">
             <h1>Experiences</h1>
-            <p>Company:<strong> GE Healthcare</strong></p>
-            <p>Role: Digital Technology Project Manager Intern</p>
-            <p>Date: 07/2021</p>
+            <div className="toppart">
+                <p>Company:<strong> GE Healthcare</strong></p>
+                <p>Role: Digital Technology Project Manager Intern</p>
+                <p>Date: 07/2021</p>
+            </div>
+            <div className="company">
+                <img
+                    src={gelogo}
+                    alt="GE Healthcare"
+                />
+            </div>
             <p>
                 <li>Lead a cross functional team on automating manual data loads and their respective data sources in all modalities within GE Healthcare (Highest Priority).</li>
                 <li>Hosted daily standups in CST and IST (India Standard Time) to communicate between teams on updates and progress every 12 hours.</li>
@@ -16,9 +26,18 @@ const About = () => {
                 <li>Initiated various meetings across various time zones to collaborate with business leaders to simplify currently complex standards of process (SOP) for data.</li>
             </p>
             <br />
-            <p>Company: <strong> i.c.stars *| MILWAUKEE </strong></p>
-            <p>Role: Resident, Technical Analyst Intern</p>
-            <p>Date: 01/2021 - Current Resident</p>
+            <div className="toppart2">
+                <p>Company: <strong> i.c.stars *| MILWAUKEE </strong></p>
+                <p>Role: Resident, Technical Analyst Intern</p>
+                <p>Date: 01/2021 - Current Resident</p>
+            </div>
+            <div className="company">
+                <img
+                    src={icstars}
+                    alt="i.c.stars |* MILWAUKEE"
+                />
+            </div>
+
             <p>
                 <li>Initialized the process of designing a wireframe, a workflow model, and using React.js to build a front-end tech portal for all of MCBC (Molson Coors Beverage Company).
                 </li>
